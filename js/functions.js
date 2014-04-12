27f8356655814d03ebec938dbb7dff94a92f5880
Hovhannes Sargsyan <sarhov@sarhov.com>
2014-04-13 03:22:55 +0400

    update
    added top hidden box, which is opening-closing click on arrow down in
    the center  of the top  navigation.

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,10 @@ $(window).load(function() {
   $(".loader").fadeOut("slow");
   $('html').css('overflow', 'visible');
 })
+
+$('.js-topBarOpen').click(function(e) {
+	e.preventDefault();
+	$(this).find('span').toggleClass('icon-arrow-up4', 'icon-arrow-down4')
+		$('.top-hidden-box').slideToggle('400');
+})
+

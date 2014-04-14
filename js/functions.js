8e313ef39aabaa1120abaa525395fad239b66a46
Hovhannes Sargsyan <sarhov@sarhov.com>
2014-04-15 00:10:55 +0400

    update

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,18 @@
+// loading while page loads
 $(window).load(function() {
   $(".loader").fadeOut("slow");
   $('html').css('overflow', 'visible');
   	$('.site-name').addClass('animated');
 })
 
+// for top hidden box
 $('.js-topBarOpen').click(function(e) {
 	e.preventDefault();
 	$(this).find('span').toggleClass('icon-arrow-up4', 'icon-arrow-down4')
 		$('.top-hidden-box').slideToggle('400');
 })
 
-
+// carousel for popular items
 $("#carouselNewProducts").carouFredSel({
 	auto : false,
 	width   : "100%",
@@ -18,7 +20,7 @@ $("#carouselNewProducts").carouFredSel({
 	next : "#carouselNewProductsNext"
 });
 
-
+// carousel for popular items
 $("#carouselPopularProducts").carouFredSel({
 	auto : false,
 	width   : "100%",
@@ -26,6 +28,12 @@ $("#carouselPopularProducts").carouFredSel({
 	next : "#carouselPopularProductsNext"
 });
 
+// nivo slider
+$(window).load(function() {
+    $('#slider').nivoSlider();
+});
 
+
+// init for animation during scrolling
 new WOW().init();
 
